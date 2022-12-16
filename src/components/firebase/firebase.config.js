d6fb1bbecb2ee471d3aee7f0f859ae0c15896603
tusarhaque <tusarhaque12@gmail.com>
2022-12-16 15:02:06 +0600

    first

---
+++ src/components/firebase/firebase.config.js
@@ -0,0 +1,11 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyClBnAC_BI2jpuU_RXaqYZ1565fNuTlAE0",
+    authDomain: "red-onion-restaurant-fa4d4.firebaseapp.com",
+    projectId: "red-onion-restaurant-fa4d4",
+    storageBucket: "red-onion-restaurant-fa4d4.appspot.com",
+    messagingSenderId: "808744863477",
+    appId: "1:808744863477:web:172b3d152d47366121a7bc"
+  };
+
+export default firebaseConfig;
